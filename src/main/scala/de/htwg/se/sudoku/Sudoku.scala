f4ae44b1d9e7c9a8eca07f8c132cbfbe60ae9883
Julian Raufelder <Julian@Raufelder.com>
2018-05-02 19:25:58 +0200

    Enahnce akka implementation

--- src/main/scala/de/htwg/se/sudoku/Sudoku.scala
+++ src/main/scala/de/htwg/se/sudoku/Sudoku.scala
@@ -1,17 +1,19 @@
 package de.htwg.se.sudoku
 
-import com.google.inject.Guice
-import de.htwg.se.sudoku.aview.Tui
+import com.google.inject.{Guice, Injector}
 import de.htwg.se.sudoku.aview.gui.SwingGui
+import de.htwg.se.sudoku.aview.{HttpServer, Tui}
 import de.htwg.se.sudoku.controller.controllerComponent.ControllerInterface
 
 import scala.io.StdIn.readLine
 
 object Sudoku {
-  val injector = Guice.createInjector(new SudokuModule)
-  val controller = injector.getInstance(classOf[ControllerInterface])
+  val injector: Injector = Guice.createInjector(new SudokuModule)
+  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
   val tui = new Tui(controller)
   val gui = new SwingGui(controller)
+  val webserver = new HttpServer(controller)
+
   controller.createNewGrid
 
   def main(args: Array[String]): Unit = {
@@ -21,5 +23,6 @@ object Sudoku {
       input = readLine()
       tui.processInputLine(input)
     } while (input != "q")
+    webserver.unbind()
   }
 }
