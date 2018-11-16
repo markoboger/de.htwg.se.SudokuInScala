0ccda398f76b1d5ebd50dc7aac66bd541f2a3df2
Julian Raufelder <Julian@Raufelder.com>
2018-11-16 22:50:32 +0100

    Added akka actors to create UIs
    Co-authored-by: DonatJR <Jonas.Reinwald@htwg-konstanz.de>

--- src/main/scala/de/htwg/se/sudoku/Sudoku.scala
+++ src/main/scala/de/htwg/se/sudoku/Sudoku.scala
@@ -2,24 +2,36 @@ package de.htwg.se.sudoku
 
 import java.awt.GraphicsEnvironment
 
+import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
+import akka.util.Timeout
 import com.google.inject.{Guice, Injector}
-import de.htwg.se.sudoku.aview.gui.SwingGui
-import de.htwg.se.sudoku.aview.{HttpServer, Tui}
+import de.htwg.se.sudoku.aview.UiMessage.{Crash, CreateGui, CreateTui}
+import de.htwg.se.sudoku.aview.{HttpServer, Tui, UiFactory}
 import de.htwg.se.sudoku.controller.controllerComponent.ControllerInterface
-import de.htwg.se.sudoku.model.fileIoComponent.FileIOInterface
 import de.htwg.se.sudoku.model.fileIoComponent.fileIoMicroImpl.FileIoHttpServer
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import scala.io.StdIn.readLine
 
 object Sudoku {
+  implicit val timeout: Timeout = Timeout(29.seconds) // used for akka ask pattern
+
   val injector: Injector = Guice.createInjector(new MicroSudokuModule)
+
+  implicit val actorSystem = ActorSystem("actorSystem")
+  val uiFactory = actorSystem.actorOf(Props[UiFactory])
+
+  uiFactory ! Crash // demonstrate error / restart handling
   val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
-  val tui = new Tui(controller)
-  
+  val tuiFuture = uiFactory ? CreateTui(controller)
+  val tui = Await.result(tuiFuture.mapTo[Tui], Duration.Inf)
+
   if (!GraphicsEnvironment.isHeadless) {
-    val gui = new SwingGui(controller)
+    uiFactory ! CreateGui(controller)
   }
-  
+
   val fileIoHttpServer: FileIoHttpServer = injector.getInstance(classOf[FileIoHttpServer])
   val webserver = new HttpServer(controller)
 
@@ -29,6 +41,8 @@ object Sudoku {
     var input: String = ""
 
     do {
+      Thread.sleep(200)
+
       if (Console.in.ready()) {
         input = readLine()
         tui.processInputLine(input)
